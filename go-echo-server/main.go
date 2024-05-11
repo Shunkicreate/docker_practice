dcbf0e4bbb8c42852f9dfbf5808c94eaadf455ad
shunki-tada <shunki.tada@dena.jp>
2024-05-11 14:48:28 +0900

    gin hotreload

--- go-echo-server/main.go
+++ go-echo-server/main.go
@@ -24,5 +24,5 @@ func main() {
 
 // Handler
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, echoでホットリロードできた!")
+	return c.String(http.StatusOK, "Hello, echo world")
 }
--- go-gin-server/main.go
+++ go-gin-server/main.go
@@ -15,7 +15,7 @@ func main() {
   })
   r.GET("/", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
-      "message": "hello world",
+      "message": "hello gin world",
     })
   })
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
